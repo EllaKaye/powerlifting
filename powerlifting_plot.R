5dfc9aede62d297f272257b3d934d663632b613c
EllaKaye <ella.kaye@gmail.com>
2025-04-08 11:34:40 +0100

    Font and ggtext

--- powerlifting_plot.R
+++ powerlifting_plot.R
@@ -1,8 +1,9 @@
 library(ggplot2)
 library(dplyr)
 library(readr)
-#library(marquee)
 library(ggtext)
+library(ragg)
+#library(marquee)
 
 # Data
 lifting_data <- read_csv("powerlifting_tracker.csv", col_types = "Dccidc")
@@ -32,7 +33,7 @@ set_colours <- c(
   heavy_3 = "#D4006A"
 )
 
-subtitle_marquee <- "Weight (kg) lifted in each set (reps): **{#6AD400 bar (10)}**, **{#00D46A light (8)}**, **{#D46A00 medium (6)}**, **{#006AD4 heavy 1 (3)}**, **{#6A00D4 heavy 2 (3)}**, and **{#D4006A heavy 3 (3)}**."
+#subtitle_marquee <- "Weight (kg) lifted in each set (reps): **{#6AD400 bar (10)}**, **{#00D46A light (8)}**, **{#D46A00 medium (6)}**, **{#006AD4 heavy 1 (3)}**, **{#6A00D4 heavy 2 (3)}**, and **{#D4006A heavy 3 (3)}**."
 
 # Create the faceted plot
 ggplot(plot_data, aes(x = date, y = weight, color = set, group = set)) +
@@ -41,7 +42,7 @@ ggplot(plot_data, aes(x = date, y = weight, color = set, group = set)) +
   facet_wrap(~lift, ncol = 1, strip.position = "top", scale = "free_y") +
   labs(
     title = "Powerlifting Progress Tracker",
-    subtitle = "Weight (kg) lifted in each set (reps): **<span style = 'color:#6AD400;'>bar (10)</span>**, **{#00D46A light (8)}**, **{#D46A00 medium (6)}**, **{#006AD4 heavy 1 (3)}**, **{#6A00D4 heavy 2 (3)}**, and **{#D4006A heavy 3 (3)}**.",
+    subtitle = "Weight (kg) lifted in each set (reps): **<span style = 'color:#6AD400;'>bar (10)</span>**, **<span style = 'color:#00D46A;'>light (8)</span>**, **<span style = 'color:#D46A00;'>medium (6)</span>**, **<span style = 'color:#006AD4;'>heavy 1 (3)</span>**, **<span style = 'color:#6A00D4;'>heavy 2 (3)</span>**, and **<span style = 'color:#D4006A;'>heavy 3 (3)</span>**",
     color = NULL
   ) +
   scale_color_manual(values = set_colours) +
@@ -52,18 +53,35 @@ ggplot(plot_data, aes(x = date, y = weight, color = set, group = set)) +
     axis.title.y = element_blank(),
     strip.text = element_text(size = 14, hjust = 0, face = "bold"), # Left-align the strip text
     #strip.background = element_rect(fill = "white", color = "gray90"),
-    text = element_text(family = "figtree"),
+    text = element_text(family = "LEMON MILK Pro"),
+    panel.spacing = unit(2, "lines"),
     plot.title.position = "plot",
-    plot.title = element_text(size = 20, margin = margin(6, 0, 12, 0)),
+    plot.title = element_text(
+      size = 20,
+      margin = margin(6, 0, 12, 0),
+      face = "bold"
+    ),
     #plot.subtitle = marquee::element_marquee(),
     plot.subtitle = element_textbox_simple(
+      size = 10.5,
       margin = margin(0, 0, 12, 0),
       lineheight = 1.3
     ),
     plot.margin = margin(rep(18, 4))
   )
 
-ggsave("powerlifting_plot.png", width = 8, height = 6, bg = "white")
+ggsave(
+  "powerlifting_plot.png",
+  width = 9,
+  height = 8,
+  bg = "white",
+  device = agg_png,
+  res = 320,
+  unit = "in"
+)
+
+
+# Total weight -----------------------------------------------------------
 
 total_weight <- lifting_data |>
   filter(!(set %in% c("one_rep", "two_rep", "heavy_4"))) |>
@@ -108,10 +126,10 @@ ggplot(lift_data, aes(date, total)) +
     text = element_text(family = "figtree"),
     plot.title.position = "plot",
     plot.title = element_text(size = 20, margin = margin(6, 0, 12, 0)),
-    #plot.subtitle = element_marquee(width = 1),
-    plot.subtitle = element_textbox_simple(
-      margin = margin(0, 0, 12, 0),
-      lineheight = 1.3
-    ),
+    plot.subtitle = element_marquee(width = 1),
+    # plot.subtitle = element_textbox_simple(
+    #   margin = margin(0, 0, 12, 0),
+    #   lineheight = 1.3
+    # ),
     plot.margin = margin(rep(18, 4))
   )
