37847a71eae49769ea9f3be06853d7e81f666c14
EllaKaye <ella.kaye@gmail.com>
2025-12-29 13:31:51 +0000

    2025-12-17

--- powerlifting_workflow.R
+++ powerlifting_workflow.R
@@ -4,11 +4,11 @@ library(ragg)
 source("powerlifting_helpers.R")
 
 # TODO: update date
-date <- "2025-12-07"
+date <- "2025-12-17"
 
 # TODO: update start_date if needed
 # To keep track of effect of menstrual cycle on lifts
-start_date <- as.Date("2025-11-13")
+start_date <- as.Date("2025-12-08")
 cycle_day <- as.numeric(as.Date(date) - start_date) + 1
 
 # TODO: update weight and maybe reps and note
@@ -19,20 +19,20 @@ latest_lifts <- tibble::tribble(
   "squat", "light", 8L, 40, NA, 
   "squat", "medium", 6L, 50, NA, 
   "squat", "heavy_1", 3L, 60, NA,
-  "squat", "heavy_2", 3L, 65, NA,
-  "squat", "heavy_3", 3L, 65, NA,
+  "squat", "heavy_2", 3L, 65, "missed depth on first couple of reps but weight felt OK",
+  "squat", "heavy_3", 3L, 67.5, "missed depth by a cm or two, but the weight felt manageable",
   "benchpress", "bar", 10L, 20, NA, 
   "benchpress", "light", 8L, 30, NA, 
-  "benchpress", "medium", 6L, 35, "feeling heavy", 
-  "benchpress", "heavy_1", 2L, 40, NA,
-  "benchpress", "heavy_2", 2L, 40, NA,
-  "benchpress", "heavy_3", 2L, NA, NA,
+  "benchpress", "medium", 6L, 35, NA, 
+  "benchpress", "heavy_1", 3L, 40, "block",
+  "benchpress", "heavy_2", 3L, 40, "block",
+  "benchpress", "heavy_3", 3L, 40, "block",
   "deadlift", "bar", 10L, NA, NA, 
   "deadlift", "light", 8L, 40, NA, 
   "deadlift", "medium", 6L, 60, NA, 
-  "deadlift", "heavy_1", 3L, 80, "weight fine but tweaked back on final rep and didn't want to risk further",
-  "deadlift", "heavy_2", 3L, NA, NA,
-  "deadlift", "heavy_3", 3L, NA, NA
+  "deadlift", "heavy_1", 3L, 80, NA,
+  "deadlift", "heavy_2", 2L, 85, NA,
+  "deadlift", "heavy_3", 3L, 85, "yes!"
 )
 
 latest_data <- cbind(date = as.Date(date), latest_lifts, cycle_day = cycle_day)
